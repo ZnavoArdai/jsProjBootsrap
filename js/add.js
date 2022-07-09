27761a29ab2af38674a99a747f10e2d446294cd6
ZnavoArdai <102303011+ZnavoArdai@users.noreply.github.com>
2022-07-09 12:06:35 +0300

    commit

---
+++ js/add.js
@@ -0,0 +1,56 @@
+
+
+const APIDEVICE="https://my-json-server.typicode.com/Jeck99/fake-server/devices";
+
+            
+
+async function getDevice(){
+    try {
+        const result=await fetch(APIDEVICE)
+        return result.json();
+    } catch (error) {
+        
+    }
+    finally{
+    }
+}
+
+getDevice().then((res)=>{console.log(res)})
+
+
+
+async function addDevice(e){
+    e.preventDefault();
+    const Data1={
+        brand:`${brandd.value}`,
+        color:`${color.value}`,
+        createdAt:`${date.value}`,
+        id:`${id_.value}`,
+        isAvailable:`${isav.value}`,
+        picture:0,
+        price:`${price.value}`,
+        ram:`${ram.value}`,
+                }
+    try {
+        return await fetch(APIDEVICE,{
+            
+            method:"POST",
+            body:JSON.stringify(Data),
+            headers:'Content-Type-application/jason'
+    
+    })
+    } catch (error) {
+        
+    }
+    finally{
+
+        for (const key in Data1) {
+     tbod_add.innerHTML+=`<tr><td>${key}</td> <td>${Data1[key]}</td></tr>`
+        }
+        
+    }
+}
+
+
+
+
--- js/main.js
+++ js/main.js
@@ -1,16 +1,1 @@
 
-function slaide(){
-
-    const burger=document.querySelector('.burger-z');
-    const navbar=document.querySelector('.navbar-z');
-const li=document.querySelectorAll('.navbar-z li')
-    burger.addEventListener("click",()=>{
-        navbar.classList.toggle('navbar-active-z');
-        li.forEach((list,index)=>{
-            list.style.animation=`slid_li 3s ease forwards ${index/4}s `
-
-        })
-
-    }) 
-}
-slaide();
